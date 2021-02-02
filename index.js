2607142a6f99a123b9f3c78170fb6853df69961c
AbdelrhmanAmin <maxhd213@gmail.com>
2021-02-03 00:14:15 +0200

    display

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let read = document.getElementById("read");
 let btn = document.querySelector(".submit");
 let table = document.getElementById("table");
 let getter = JSON.parse(localStorage.getItem("library"));
+myLibrary.push(...getter);
 console.log(getter);
 function Book(author, title, pages, read = false) {
   this.author = author;
@@ -20,28 +21,25 @@ function addBookToLibrary(arr, book) {
 
 btn.addEventListener("click", () => {
   if (auth.value !== "" && title.value !== "" && pages.value !== "") {
-    let newBook = new Book(auth.value, title.value, pages.value, read.checked);
-    auth.value = "";
-    title.value = "";
-    pages.value = 0;
-    read.checked = false;
-    addBookToLibrary(myLibrary, newBook);
+    let book = new Book(auth.value, title.value, pages.value, read.checked);
+    addBookToLibrary(myLibrary, book);
     myLibrary.push(...getter);
     localStorage.setItem("library", JSON.stringify(myLibrary));
-  }
-});
-function displayEachBook() {
-  // let td_btn = document.createElement("td");
-  // let del_btn = document.createElement("button");
-  // del_btn.innerHTML = "Delete";
-  // del_btn.className = "btn btn-danger";
-  // td_btn.appendChild(del_btn);
-  getter.forEach((book) => {
     let tr = document.createElement("tr");
     let td_Author = document.createElement("td");
     let td_Title = document.createElement("td");
     let td_Pages = document.createElement("td");
     let td_Read = document.createElement("td");
+    let td_btn = document.createElement("td");
+    let del_btn = document.createElement("button");
+    del_btn.innerHTML = "Delete";
+    del_btn.className = "btn btn-danger";
+    del_btn.addEventListener("click", () => {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      localStorage.setItem("library", JSON.stringify(myLibrary));
+      tr.innerHTML = "";
+    });
+    td_btn.appendChild(del_btn);
     td_Author.innerHTML = book.author;
     td_Title.innerHTML = book.title;
     td_Pages.innerHTML = book.pages;
@@ -50,9 +48,41 @@ function displayEachBook() {
     tr.appendChild(td_Title);
     tr.appendChild(td_Pages);
     tr.appendChild(td_Read);
+    tr.appendChild(td_btn);
+    table.appendChild(tr);
+    auth.value = "";
+    title.value = "";
+    pages.value = 0;
+    read.checked = false;
+  }
+});
+function displayEachBook(getter) {
+  getter.forEach((book) => {
+    let tr = document.createElement("tr");
+    let td_Author = document.createElement("td");
+    let td_Title = document.createElement("td");
+    let td_Pages = document.createElement("td");
+    let td_Read = document.createElement("td");
+    let td_btn = document.createElement("td");
+    let del_btn = document.createElement("button");
+    del_btn.innerHTML = "Delete";
+    del_btn.className = "btn btn-danger";
+    del_btn.addEventListener("click", () => {
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+      localStorage.setItem("library", JSON.stringify(myLibrary));
+      tr.innerHTML = "";
+    });
+    td_btn.appendChild(del_btn);
+    td_Author.innerHTML = book.author;
+    td_Title.innerHTML = book.title;
+    td_Pages.innerHTML = book.pages;
+    td_Read.innerHTML = book.read;
+    tr.appendChild(td_Author);
+    tr.appendChild(td_Title);
+    tr.appendChild(td_Pages);
+    tr.appendChild(td_Read);
+    tr.appendChild(td_btn);
     table.appendChild(tr);
   });
-
-  // tr.appendChild(td_btn);
 }
-displayEachBook();
+displayEachBook(getter);
