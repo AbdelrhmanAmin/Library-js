fc06e4d83e2f4af98e0bc3061d0c0b920b67eda8
AbdelrhmanAmin <maxhd213@gmail.com>
2021-02-03 23:44:07 +0200

    Refactor to use classes

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 let myLibrary = [];
-function Book(author, title, pages, read = false) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-}
-
-function setBooks() {
-  const data = JSON.stringify(myLibrary);
-  localStorage.setItem('library', data);
-}
-
-function getBooks() {
-  let data = localStorage.getItem('library');
-  data = JSON.parse(data);
-  if (data == null) {
-    myLibrary.push(new Book('Neko Master', '9999 Reasons I love my mommy', '9999999', true));
-    setBooks();
-    data = JSON.parse(localStorage.getItem('library'));
+class Book {
+  constructor(author, title, pages, read = false) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
   }
-  return data;
-}
+  setBooks() {
+    const data = JSON.stringify(myLibrary);
+    localStorage.setItem('library', data);
+  }
 
-function addBookToLibrary(arr, book) {
-  arr.push(book);
-  setBooks();
-  window.location.reload();
+  getBooks() {
+    let data = localStorage.getItem('library');
+    data = JSON.parse(data);
+    if (data == null) {
+      myLibrary.push(
+        new Book('Neko Master', '9999 Reasons I love my mommy', '9999999', true)
+      );
+      setBooks();
+      data = JSON.parse(localStorage.getItem('library'));
+    }
+    return data;
+  }
+
+  addBookToLibrary(arr, book) {
+    arr.push(book);
+    setBooks();
+    window.location.reload();
+  }
 }
 
 const getter = getBooks();
