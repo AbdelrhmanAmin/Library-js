b5b0557d04c9b1088abfec7a02c3a605997c3f08
AbdelrhmanAmin <maxhd213@gmail.com>
2021-02-03 23:55:27 +0200

    Modify classes

--- index.js
+++ index.js
@@ -6,34 +6,33 @@ class Book {
     this.pages = pages;
     this.read = read;
   }
-  setBooks() {
-    const data = JSON.stringify(myLibrary);
-    localStorage.setItem('library', data);
-  }
-
-  getBooks() {
-    let data = localStorage.getItem('library');
-    data = JSON.parse(data);
-    if (data == null) {
-      myLibrary.push(
-        new Book('Neko Master', '9999 Reasons I love my mommy', '9999999', true)
-      );
-      setBooks();
-      data = JSON.parse(localStorage.getItem('library'));
-    }
-    return data;
-  }
-
   addBookToLibrary(arr, book) {
     arr.push(book);
     setBooks();
     window.location.reload();
   }
 }
+function setBooks() {
+  const data = JSON.stringify(myLibrary);
+  localStorage.setItem('library', data);
+}
+
+function getBooks() {
+  let data = localStorage.getItem('library');
+  data = JSON.parse(data);
+  if (data == null) {
+    myLibrary.push(
+      new Book('Neko Master', '9999 Reasons I love my mommy', '9999999', true)
+    );
+    setBooks();
+    data = JSON.parse(localStorage.getItem('library'));
+  }
+  return data;
+}
 
 const getter = getBooks();
 myLibrary = [...getter];
-
+console.log(getter);
 const auth = document.getElementById('author');
 const title = document.getElementById('title');
 const pages = document.getElementById('pages');
@@ -44,6 +43,6 @@ btn.addEventListener('click', (e) => {
   e.preventDefault();
   if (auth.value !== '' && title.value !== '' && pages.value !== '') {
     const book = new Book(auth.value, title.value, pages.value, read.checked);
-    addBookToLibrary(myLibrary, book);
+    book.addBookToLibrary(myLibrary, book);
   }
 });
